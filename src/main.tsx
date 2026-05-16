1530f2a07f71b65b0bf16ea62ebf125d3f6c5da6
Andrii-Mitko <andreymit123@gmail.com>
2026-05-16 09:59:45 +0300

    rename components temp

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./components/App/App";
+import App from "./temp-components/App/App";
 import "modern-normalize";
 import "./index.css";
 
